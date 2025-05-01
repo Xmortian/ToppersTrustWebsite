2325f7b899663d2fbcc3dee998e69c33557943ca
Xmortian <nafis40m0@gmail.com>
2025-05-02 03:12:18 +0600

    Initial commit

---
+++ app/out/vs/base/common/worker/simpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/core/vs/base/common/worker/simpleWorker.nls.js.map
---
+++ app/out/vs/workbench/contrib/debug/node/telemetryApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/workbench/contrib/debug/node/telemetryApp.nls",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/core/vs/workbench/contrib/debug/node/telemetryApp.nls.js.map
